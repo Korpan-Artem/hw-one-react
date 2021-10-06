f3b6067e3ca4e6312a84e6343263fc2143d8ef57
Artem Korpan <arttodoc@gmail.com>
2021-10-06 12:28:45 +0300

    made empty project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import './themes/App.css';
-import user from './user.json';
-import friends from './friends.json';
-import { Profile } from './components/Profile';
-import { FriendList } from './components/FriendList';
+
 
 function App() {
   return (
     <>
-    <Profile 
-      name={user.name} 
-      tag={user.tag} 
-      location={user.location} 
-      avatar={user.avatar} 
-      stats={user.stats}
-    />
-    <FriendList friends={friends}/>
     </>
   );
 }
