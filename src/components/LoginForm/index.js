94bff6380e0a2931e22aa62e0ba6c4e3f587dba2
Artem Korpan <arttodoc@gmail.com>
2021-10-09 11:01:53 +0300

    update hw-2

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,64 +1,21 @@
 import React from "react";
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 export const LoginForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            email:"foobar@example.com",
+            password:"foobar",
+        },
+        onSubmit: (values) => {
+           alert(JSON.stringify(values,null,2));
+        },
+    });
     return (
-    <div>
-     <h1>Anywhere in your app!</h1>
-     <Formik
-       initialValues={{ email: '', password: '' }}
-       placeholder =' email'
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-         <form onSubmit={handleSubmit}>
-           <input
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-           />
-           {errors.email && touched.email && errors.email}
-           <input
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-           />
-           {errors.password && touched.password && errors.password}
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </form>
-       )}
-     </Formik>
-   </div>
+        <form  className="block-form" onSubmit={formik.handleSubmit}>
+            <input className="input-1" name="email" value={formik.values.email} onChange={formik.handleChange} type="email" />
+            <input className="input-2" name="password" value={formik.values.password} onChange={formik.handleChange} type="password" />
+            <button className="btn-submit" type="submit">Submit</button>
+        </form>
     )
 }
 
--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,11 +1,67 @@
 import React from 'react';
-
+import { useFormik, Formik } from 'formik';
+import * as Yup from 'yup';
 export const SignUpForm = () => {
+    const validation = Yup.object().shape({
+        username: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+         password: Yup.number()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+        email: Yup.string().email('Invalid email').required('Required'),
+      });
+    const formik = useFormik({
+        initialValues: {
+            name:"",
+            email:"",
+            password:"",
+        },
+        validationSchema:{validation},
+        onSubmit: (values) => {
+           alert(JSON.stringify(values,null,2));
+        },
+        
+    });
     return (
-        <>
-        <h1>SingUpForm</h1>
-        </>
+        <Formik  className="block-form" onSubmit={formik.handleSubmit} >
+          {({values,erorrs, touched, handleChange,handleBlur,isValid,handleSubmit,dirty}) => (
+              <>
+                    <label>Name</label>
+                    <input 
+                        className="input-1" 
+                        name="name" 
+                        value={formik.values.name} 
+                        onChange={handleChange} 
+                        onBlur={handleBlur} 
+                        type="text" 
+                    />
+                    <input className="input-2" name="email" value={formik.values.email} onChange={handleChange} type="email" />
+                    <input className="input-2" name="password" value={formik.values.password} onChange={handleChange} type="password" />
+                    <button className="btn-submit" type="submit">Submit</button>
+            </>
+          )}
+        </Formik>
+        
     )
 }
 
 export default SignUpForm;
+
+/*
+                
+            <label>Name</label>
+            <input 
+                className="input-1" 
+                name="name" 
+                value={values.name} 
+                onChange={handleChange} 
+                onBlur={handlerBlur} 
+                type="text" 
+            />
+                <input className="input-2" name="email" value={formik.values.email} onChange={formik.handleChange} type="email" />
+                <input className="input-2" name="password" value={formik.values.password} onChange={formik.handleChange} type="password" />
+                <button className="btn-submit" type="submit">Submit</button>
+            */
